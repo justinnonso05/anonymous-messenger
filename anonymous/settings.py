e822c4b6f42c11bfb843974e435e84a981a77a3e
justinnonso05 <chinonsoali2005@gmail.com>
2025-04-08 16:02:17 +0100

    static files

--- anonymous/settings.py
+++ anonymous/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +130,8 @@ LOGIN_URL = 'login'
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'home'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
